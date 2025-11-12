e26af582d42cf02da384b9f686543a72d2af1359
reshdrai <141801808+reshdrai@users.noreply.github.com>
2025-11-12 19:35:25 +0545

    Second commit with frontend updated

--- FrontendBananaApi_Game/src/App.jsx
+++ FrontendBananaApi_Game/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import './App.css'; // we'll create this next
+import './App.css'; // we'll create this next
 
 function App() {
   const [username, setUsername] = useState('');
